ee14953415c0cdef5ec84e75f52e1209f7f0a05c
Michael P. <m.polazhynets@hotmail.com>
2025-11-09 21:15:09 +0200

    fix: move Firebase config to environment variables

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDgt-GDl8_rQgFRdmm9KV1RR-fbfatIgpQ",
-  authDomain: "settle-4af7c.firebaseapp.com",
-  projectId: "settle-4af7c",
-  storageBucket: "settle-4af7c.firebasestorage.app",
-  messagingSenderId: "397786014586",
-  appId: "1:397786014586:web:e9f6ff6dd2eee83c83ecc3",
-  measurementId: "G-FTHFWBHTLB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
